d4cc763cde6369a9144e22b31433e5e9a29379b7
DennY <45600211+DennYats@users.noreply.github.com>
2020-10-29 13:40:17 +0200

    Update Program.cs

--- sprint 01/Sprint01/Program.cs
+++ sprint 01/Sprint01/Program.cs
@@ -6,7 +6,7 @@ namespace Sprint01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Method Main!");
         }
     }
 }
